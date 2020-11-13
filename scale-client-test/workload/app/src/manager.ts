ebe980666942183028ccf3f352425981c9172856
tetsu5555 <tetete1118@gmail.com>
2020-11-13 13:06:38 +0900

    fix manager

--- scale-client-test/workload/app/src/manager.ts
+++ scale-client-test/workload/app/src/manager.ts
@@ -1,10 +1,9 @@
 const k8s = require('@kubernetes/client-node');
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
 
 const getPodNames = async () => {
-  const kc = new k8s.KubeConfig();
-  kc.loadFromDefault();
   const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
   const res = await k8sApi.listNamespacedPod('default')
   const names = res.body.items.map((item: any) => {
     return item.metadata.name
@@ -13,33 +12,30 @@ const getPodNames = async () => {
 }
 
 let count = 2
+const scale = async (namespace: string, name: string, replicas: number) => {
+  const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
+  // find the particular deployment
+  const res = await k8sApi.readNamespacedDeployment(name, namespace);
+  let deployment = res.body;
+  // console.log('deployment', deployment)
+
+  // edit
+  deployment.spec.replicas = replicas;
+
+  // replace
+  await k8sApi.replaceNamespacedDeployment(name, namespace, deployment);
+
+  console.log(`pod scaled to ${count}`)
+  count++
+}
+
+
 
 const runManager = async () => {
   await getPodNames()
 
-  const kc = new k8s.KubeConfig();
-  kc.loadFromDefault();
-
-  const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
-
   const targetDeploymentName = 'scale-app-worker';
-
-  async function scale(namespace: string, name: string, replicas: number) {
-    // find the particular deployment
-    const res = await k8sApi.readNamespacedDeployment(name, namespace);
-    let deployment = res.body;
-    console.log('deployment', deployment)
-
-    // edit
-    deployment.spec.replicas = replicas;
-
-    // replace
-    await k8sApi.replaceNamespacedDeployment(name, namespace, deployment);
-  }
-
-  scale('default', 'docker-test-deployment', count);
-  console.log(`pod scaled to ${count}`)
-  count++
+  scale('default', targetDeploymentName, count);
 };
 
 const runMassPushV3Manager = async ({ interval_msec }: { interval_msec: number }) => {
